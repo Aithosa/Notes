0d06d49c107481d66928fe785af23c22d100bed5
Aithusa <32020141+Aithosa@users.noreply.github.com>
2018-10-12 11:08:04 +0800

    modify yesterday's code

--- leetcode/Longest_Common_Prefix.py
+++ leetcode/Longest_Common_Prefix.py
@@ -8,10 +8,12 @@ class Solution:
             return ''
         
         strs.sort()
+        # 排序之后第一个和最后一个就是差异最大的一对
         first = strs[0]
         last = strs[-1]
         
         i = 0
+        # 可以用个最小值
         while i < len(first) and i < len(last) and first[i] == last[i]:
             i += 1
         return first[:i]
@@ -33,3 +35,50 @@ class Solution:
             else:
                 return result
         return result
+
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        cstr = ""
+        idx = 0
+        end = 0
+        is_break = False
+        while True:
+            if len(strs[0]) > idx:
+                cstr = strs[0][idx]
+            else:
+                break
+            for i in range(1,len(strs)):
+                if strs[i] == "":
+                    is_break =True
+                    break
+                elif idx < len(strs[i]) and cstr == strs[i][idx]:
+                    continue
+                else:
+                    is_break = True
+                    break
+            if is_break:
+                break
+            idx += 1
+            end = idx
+        return strs[0][:end]
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        l=min([len(t) for t in strs],default=0)
+        if l==0:
+            return ""
+        if len(strs)==1:
+            return strs[0]
+        init=strs[0]
+        for i in range(l):
+            for s in strs[1:]:
+                if s[i]!=init[i]:
+                    return init[:i]
+        return init[:l]
